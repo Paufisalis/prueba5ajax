0dc8e981069b31f7ac51822b1d5c98e8247d72a8
paulina <paulinafs89@gmail.com>
2019-10-15 14:17:08 -0300

    cambios en complaint

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :complaints, except: [:create]
   resources :companies do
-  resources :complaints, only: [:create]
+  resources :complaints
   end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
